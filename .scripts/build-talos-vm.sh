2ea21016b323c8a31bdb2360fb376ebfefa11bcb
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-08-11 15:27:03 +0100

    mvp: create a talos-vm that is assigned an ipv4 addr

--- .scripts/build-talos-vm.sh
+++ .scripts/build-talos-vm.sh
@@ -2,19 +2,44 @@
 set -e
 
 VM_NAME="talos-vm"
-ISO="talos-amd64.iso"
+ISO_PATH="talos-amd64.iso"
 ISO_VOL="talos-iso-vol"
+ISO_URL="https://factory.talos.dev/image/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba/v1.10.6/metal-amd64.iso"
+ISO_SHA="f8d38dddbd74e32853980a1775f67d65b7cd43a88b3134abe900ac21705f085b"
 STORAGE_POOL="default"
+
+if [ ! -f "${ISO_PATH}" ]; then
+  curl -o "${ISO_PATH}" "${ISO_URL}"
+fi
+
+if [ "${ISO_SHA}" != "$(sha256sum "${ISO_PATH}" | awk '{print $1}')" ]; then
+  echo "WARNING: '${ISO_PATH}' has an unexpected SHA256SUM!"
+  exit 1
+fi
 
 # 1)
 echo "preparing talos VM using incus"
-incus init "${VM_NAME}" --empty --vm -c limits.cpu=4 -c limits.memory=4GiB -d root,size=50GiB
+incus init "${VM_NAME}" \
+	--empty --vm \
+	-c limits.cpu=4 -c limits.memory=4GiB -c security.secureboot=false \
+	-d root,size=50GiB
 
 echo "import talos ISO into incus as storage vol"
-incus storage volume import "${STORAGE_POOL}" "${ISO}" "${ISO_VOL}" --type=iso
+incus storage volume import "${STORAGE_POOL}" "${ISO_PATH}" "${ISO_VOL}" --type=iso
 
 echo "attach ISO storage vol to VM"
 incus config device add "${VM_NAME}" "${ISO_VOL}" disk pool="${STORAGE_POOL}" source="${ISO_VOL}" boot.priority=10
+
+cat <<EOF
+# Next steps #
+
+# interact with the VM's boot-menu:
+incus start ${VM_NAME} --console
+
+# detach the ISO volume from the storage pool
+incus storage volume detach ${STORAGE_POOL} ${ISO_VOL} ${VM_NAME}
+
+EOF
 
 
 # REFERENCES #
